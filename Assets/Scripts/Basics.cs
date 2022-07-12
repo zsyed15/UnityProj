62ec47e05cb05ba00571c724b2df14cc6e021d9e
Zafeer Syed <zsyed@ucsd.edu>
2022-07-11 21:20:41 -0700

    initial commit

---
+++ Assets/Scripts/Basics.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basics : MonoBehaviour
+{
+    float speed = 1.4f;
+    double mana = 15.5;
+    int health = 100;
+    string playerName = "Warrior";
+    bool isDead = true;
+
+        
+    }
+}
